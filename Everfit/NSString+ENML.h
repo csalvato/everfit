6c2847ae4bc3d1667622dbef28aa0dc87155408f
Chris Salvato <csalvato@gmail.com>
2012-08-30 18:04:13 +0100

    Refactored ENML Category to include other ENML helper methods.

---
+++ Everfit/NSString+ENML.h
@@ -0,0 +1,13 @@
+//
+//  NSString+ENML.h
+//  Everfit
+//
+//  Created by Chris Salvato on 8/30/12.
+//  Copyright (c) 2012 Swift Archer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (ENML)
++(NSString *)encapulateStringInENML:(NSString *)string;
+@end
--- Everfit/NSString+ENML.m
+++ Everfit/NSString+ENML.m
@@ -1,14 +1,14 @@
 //
-//  NSString+EncapsulateInENML.m
+//  NSString+ENML.m
 //  Everfit
 //
 //  Created by Chris Salvato on 8/30/12.
 //  Copyright (c) 2012 Swift Archer. All rights reserved.
 //
 
-#import "NSString+EncapsulateInENML.h"
+#import "NSString+ENML.h"
 
-@implementation NSString (EncapsulateInENML)
+@implementation NSString (ENML)
 
 +(NSString *)encapulateStringInENML:(NSString *)string {
     return [NSString stringWithFormat: @"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\">\n<en-note>%@</en-note>", string];
