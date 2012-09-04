fecda9f147d03ac7993ca9d1b8385f93be6de35f
Chris Salvato <csalvato@gmail.com>
2012-09-04 15:22:54 +0100

    Created more skeletons for test framework.  Removed unnecessary files that were auto-generated by Xcode.

---
+++ EverfitTests/NSString+ENMLTests.h
@@ -0,0 +1,13 @@
+//
+//  NSString+ENMLTests.h
+//  Everfit
+//
+//  Created by Chris Salvato on 9/4/12.
+//  Copyright (c) 2012 Swift Archer. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSString_ENMLTests : SenTestCase
+
+@end
---
+++ EverfitTests/NSString+ENMLTests.m
@@ -0,0 +1,69 @@
+//
+//  NSString+ENMLTests.m
+//  Everfit
+//
+//  Created by Chris Salvato on 9/4/12.
+//  Copyright (c) 2012 Swift Archer. All rights reserved.
+//
+
+#import "NSString+ENMLTests.h"
+
+@implementation NSString_ENMLTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testConvertTextViewFormatToENML
+{
+    STFail(@"Not Yet Implemented");
+}
+
+- (void)testConvertDoubleNewLinesToBR {
+    STFail(@"Not Yet Implemented");
+}
+
+- (void)testConvertNewLinesToDivs {
+    STFail(@"Not Yet Implemented");
+}
+
+- (void)testEncapsulateStringInENML
+{
+    STFail(@"Not Yet Implemented");
+}
+
+- (void)testConvertENMLtoTextViewFormat
+{
+    STFail(@"Not Yet Implemented");
+}
+
+- (void)testConvertDivTagsToNewLines
+{
+    STFail(@"Not Yet Implemented");
+}
+
+
+- (void)testConvertBRTagsToNewLines
+{
+    STFail(@"Not Yet Implemented");
+}
+
+- (void)testStripENNoteTags
+{
+    STFail(@"Not Yet Implemented");
+}
+
+
+
+
+@end
